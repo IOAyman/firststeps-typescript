bf7dc200b7ef91d4f89a8ac3f05c9c06a48f1d88
IOAyman <theycallmethedr@gmail.com>
2016-06-23 23:30:24 +0100

    three docs

--- src/three.module.ts
+++ src/three.module.ts
@@ -1,13 +1,16 @@
+// exporting a class
 export class PartOne {
     constructor (name?:string) {
         console.log("PartOne instantiated by [" + (name||'Unknown') + "]")
     }
 }
 
+// exporting a class
 export class PartTwo {
     constructor() { console.log('PartTwo instantiated') }
 }
 
+// exporting a **Default** class
 export default class PartZero {
     constructor() { console.log('Part ZERO is liiiiive :D') }
 }
--- src/three.ts
+++ src/three.ts
@@ -1,6 +1,9 @@
-
+// import a class from a separate module
+// of course, you must not include .ts!
 import { PartOne } from './three.module'
+// using the 'as' thing
 import { PartTwo as PT } from './three.module'
+// this is how you import the **Default** exported class
 import PartZero from './three.module'
 
 new PartOne()
