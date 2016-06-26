58359b79e531022b9c0a478d46b37bab5c787187
IOAyman <theycallmethedr@gmail.com>
2016-06-26 16:43:08 +0100

    added build and default tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,55 @@
 var gulp = require('gulp')
 var tsc = require('gulp-typescript')
+var uglify = require('gulp-uglify')
 
 
+// projects config
 var basics = {
     "project": tsc.createProject('./basics/tsconfig.json'),
     "src": "./basics/*.ts",
-    "dest": "./build/basics"
+    "build": "./build/basics",
+    "dist": "./dist/basics"
 }
-
 var node = {
     "project": tsc.createProject('./node/tsconfig.json', {
                     module: "commonjs"
                 }),
     "src": ["./node/*.ts", "./node/express/*.ts"],
-    "dest": "./build/node"
+    "build": "./build/node",
+    "dist": "./dist/node"
 }
 
 
 gulp.task('tsc-basics', function() {
     return gulp.src(basics.src)
       .pipe(tsc(basics.project))
-      .pipe(gulp.dest(basics.dest));
+      .pipe(gulp.dest(basics.build));
 });
-
 gulp.task('tsc-node', function() {
     return gulp.src(node.src)
       .pipe(tsc(node.project))
-      .pipe(gulp.dest(node.dest));
+      .pipe(gulp.dest(node.build));
+});
+
+
+gulp.task('build-basics', ['tsc-basics'], function() {
+    return gulp.src(basics.build + '**/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest(basics.dist));
+});
+gulp.task('build-node', ['tsc-node'], function() {
+    return gulp.src(node.build + '**/*.js')
+      .pipe(uglify())
+      .pipe(gulp.dest(node.dist));
 });
 
 
 gulp.task('basics', function() {
     return gulp.watch(basics.src, ['tsc-basics']);
 });
-
 gulp.task('node', function() {
     return gulp.watch(node.src, ['tsc-node']);
 });
+
+
+gulp.task('default',['build-basics', 'build-node'], function() {});
