2faf4c594f9b0f27e0e812973e3f305864d6708a
IOAyman <theycallmethedr@gmail.com>
2016-07-06 17:17:41 +0100

    added serve task
    renamed tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-var gulp = require('gulp')
-var tsc = require('gulp-typescript')
-var uglify = require('gulp-uglify')
+var gulp = require('gulp');
+var tsc = require('gulp-typescript');
+var uglify = require('gulp-uglify');
+var nodemon = require('gulp-nodemon');
 
 
 // projects config
@@ -20,24 +21,24 @@ var node = {
 }
 
 
-gulp.task('tsc-basics', function() {
+gulp.task('tsc:basics', function() {
     return gulp.src(basics.src)
       .pipe(tsc(basics.project))
       .pipe(gulp.dest(basics.build));
 });
-gulp.task('tsc-node', function() {
+gulp.task('tsc:node', function() {
     return gulp.src(node.src)
       .pipe(tsc(node.project))
       .pipe(gulp.dest(node.build));
 });
 
 
-gulp.task('build-basics', ['tsc-basics'], function() {
+gulp.task('build:basics', ['tsc:basics'], function() {
     return gulp.src(basics.build + '**/*.js')
       .pipe(uglify())
       .pipe(gulp.dest(basics.dist));
 });
-gulp.task('build-node', ['tsc-node'], function() {
+gulp.task('build:node', ['tsc:node'], function() {
     return gulp.src(node.build + '/**/*.js')
       .pipe(uglify())
       .pipe(gulp.dest(node.dist));
@@ -45,11 +46,15 @@ gulp.task('build-node', ['tsc-node'], function() {
 
 
 gulp.task('basics', function() {
-    return gulp.watch(basics.src, ['tsc-basics']);
+    return gulp.watch(basics.src, ['tsc:basics']);
 });
-gulp.task('node', function() {
-    return gulp.watch(node.src, ['tsc-node']);
+gulp.task('node', ['serve'], function() {
+    return gulp.watch(node.src, ['tsc:node']);
 });
 
 
-gulp.task('default',['build-basics', 'build-node'], function() {});
+gulp.task('serve',['build:node'], function() {
+    nodemon({ script: './build/node/express/express.js' })
+});
+
+gulp.task('default',['build:basics', 'build:node'], function() {});
